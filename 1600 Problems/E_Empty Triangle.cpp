f2ea8d80d9939fc5354e4e9eed8ac23d076fee04
Rahul Singh <85445665+rahulsingh522003@users.noreply.github.com>
2025-04-26 11:32:53 +0530

    Create E_Empty Triangle.cpp

---
+++ 1600 Problems/E_Empty Triangle.cpp
@@ -0,0 +1,97 @@
+// Problem Link - https://codeforces.com/problemset/problem/2074/E
+
+// Solution
+
+
+
+/* rsingh2003 */
+
+// Don't look at my code Bitch !!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// pbds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T>using oset = tree<T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>; // order_of_key  find_by_order
+
+#define int long long
+
+/*********************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*********************************************************************************************************/
+
+// I know it's hard, that's why I'm doing this. 
+
+void rahul()
+{
+    int n;
+    cin >> n;
+    int f = 1, m = n/2+1, l = n;
+    // int cnt = 0;
+    while(true)
+    {
+        cout << "? " << f << " " << m << " " << l << endl;
+        int ab;
+        cin >> ab;
+        if(ab == 0)
+        {
+            cout << "! " << f << " " << m << " " << l << endl;
+            break;
+        }
+        int cnt = rand()%3;
+        if(cnt == 0)
+        {
+            l = ab;
+        }
+        else if(cnt == 1)
+        {
+            m = ab;
+        }
+        else
+        {
+            f = ab;
+        }
+        // cnt++;
+    }
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    srand(time(NULL));
+    cout << fixed << setprecision(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        rahul();
+    }
+    return 0;
+}
+
+
+
+
