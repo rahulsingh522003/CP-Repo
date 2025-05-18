151bb43e007bb23c8a519dc28aa4300bbca03e8f
Rahul Singh <85445665+rahulsingh522003@users.noreply.github.com>
2025-05-18 23:43:54 +0530

    Create F_Goodbye, Banker Life.cpp

---
+++ 1700 Problem/F_Goodbye, Banker Life.cpp
@@ -0,0 +1,89 @@
+// Problem Link - https://codeforces.com/problemset/problem/2072/F
+
+// Solution
+
+
+
+/* rsingh2003 */
+
+// Don't look at my code Bitch !!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// pbds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T>using oset = tree<T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>; // order_of_key  find_by_order
+
+#define int long long
+
+/*********************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*********************************************************************************************************/
+
+// I know it's hard, that's why I'm doing this. 
+
+
+void rahul()
+{
+    int n,k;
+    cin >> n >> k;
+    vector<int>v(n);
+    for(int i = 0; i < n; i++)
+    {
+        // int ab = ncr(n-1,i);
+        int c1 = 0;
+        for(int j = 2; j <= n-1; j *= 2)
+            c1 = c1 + ((n-1)/j);
+        int c2 = 0;
+        int c3 = 0;
+        for(int j = 2; j <= i; j *= 2)
+            c2 = c2 + ((i)/j);
+        for(int j = 2; j <= (n-i-1); j *= 2)
+            c3 = c3 + (n-i-1)/j;
+        if(c1 == c2 + c3)
+            cout << k << " ";
+        else
+            cout<< 0 << " ";
+    }
+    cout << '\n';
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(6);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        rahul();
+    }
+    return 0;
+}
+
+
+
+
