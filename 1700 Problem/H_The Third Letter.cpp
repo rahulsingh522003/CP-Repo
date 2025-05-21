335df8e2cf74366bd0d0a5f6fa1bd3f4294b616c
Rahul Singh <85445665+rahulsingh522003@users.noreply.github.com>
2025-05-21 10:48:59 +0530

    Create H_The Third Letter.cpp

---
+++ 1700 Problem/H_The Third Letter.cpp
@@ -0,0 +1,114 @@
+// Problem Link - https://codeforces.com/problemset/problem/1850/H
+
+// Solution
+
+
+
+/* rsingh2003 */
+
+// Don't look at my code Bitch !!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// pbds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T>using oset = tree<T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>; // order_of_key  find_by_order
+
+#define int long long
+
+/*********************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*********************************************************************************************************/
+
+// I know it's hard, that's why I'm doing this. 
+
+void dfs(vector<pair<int,int>>adj[], int sr, vector<int>&vis, map<int,int>&mp)
+{
+    vis[sr] = 1;
+    for(auto &it : adj[sr])
+    {
+        if(vis[it.first] == 0)
+        {
+            mp[it.first] = mp[sr] + it.second;
+            dfs(adj,it.first,vis,mp);
+        }
+    }
+}
+
+void rahul()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<pair<int,int>>adj[n+1];
+    vector<vector<int>>v;
+    for(int i = 0; i < m; i++)
+    {
+        int a,b,d;
+        cin >> a >> b >> d;
+        v.push_back({a,b,d});
+        adj[b].push_back({a,d});
+        adj[a].push_back({b,-d});
+    }
+    map<int,int>mp;
+    vector<int>vis(n+1);
+    for(int i = 1; i <= n; i++)
+    {
+        if(vis[i] == 0)
+        {
+            mp[i] = 0;
+            dfs(adj,i,vis,mp);
+        }
+    }
+    for(int i = 0; i < m; i++)
+    {
+        int a = v[i][0];
+        int b = v[i][1];
+        int d = v[i][2];
+        if(mp[a] - mp[b] != d)
+        {
+            cout << "NO" << '\n';
+            return;
+        }
+    }
+    cout << "YES" << '\n';
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(6);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        rahul();
+    }
+    return 0;
+}
+
+
+
+
