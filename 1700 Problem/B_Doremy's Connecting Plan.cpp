27d435787607ec8d1be0df2c5c39e82eb78cf83d
Rahul Singh <85445665+rahulsingh522003@users.noreply.github.com>
2025-05-22 09:44:15 +0530

    Create B_Doremy's Connecting Plan.cpp

---
+++ 1700 Problem/B_Doremy's Connecting Plan.cpp
@@ -0,0 +1,87 @@
+// Problem Link - https://codeforces.com/problemset/problem/1889/B
+
+// Solution
+
+
+
+/* rsingh2003 */
+
+// Don't look at my code Bitch !!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// pbds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T>using oset = tree<T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>; // order_of_key  find_by_order
+
+#define int long long
+
+/*********************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*********************************************************************************************************/
+
+// I know it's hard, that's why I'm doing this. 
+
+void rahul()
+{
+    int n,c;
+    cin >> n >> c;
+    vector<int>v(n);
+    for(auto &it : v)
+        cin >> it;
+    vector<pair<int,int>>vp;
+    for(int i = 1; i < n; i++)
+        vp.push_back({v[i]-c*(i+1),v[i]});
+    sort(vp.rbegin(), vp.rend());
+    int sum = v[0];
+    for(int i = 0; i < n-1; i++)
+    {
+        if(sum + vp[i].first < 0)
+        {
+            cout << "NO" << '\n';
+            return;
+        }
+        sum = sum + vp[i].second;
+    }
+    cout << "YES" << '\n';
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(6);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        rahul();
+    }
+    return 0;
+}
+
+
+
+
