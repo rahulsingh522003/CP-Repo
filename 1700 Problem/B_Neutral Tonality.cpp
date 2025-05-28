880da4bd63bdecf74a18d9a19b8680eaeeba7cfc
Rahul Singh <85445665+rahulsingh522003@users.noreply.github.com>
2025-05-28 13:49:36 +0530

    Create B_Neutral Tonality.cpp

---
+++ 1700 Problem/B_Neutral Tonality.cpp
@@ -0,0 +1,101 @@
+// Problem Link - https://codeforces.com/problemset/problem/1893/B
+
+// Solution
+
+
+
+/* rsingh2003 */
+
+// Don't look at my code Bitch !!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// pbds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T>using oset = tree<T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>; // order_of_key  find_by_order
+
+#define int long long
+
+/*********************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*********************************************************************************************************/
+
+// I know it's hard, that's why I'm doing this. 
+
+void rahul()
+{
+    int n,m;
+    cin >> n >> m;
+    vector<int>a(n);
+    for(auto &it : a)
+        cin >> it;
+    vector<int>b(m);
+    for(auto &it : b)
+        cin >> it;
+    sort(b.rbegin(), b.rend());
+    int i {}, j {};
+    while(i < n && j < m)
+    {
+        if(a[i] <= b[j])
+        {
+            cout << b[j] <<" ";
+            j++;
+        }
+        else
+        {
+            cout << a[i] << " ";
+            i++; 
+        }
+    }
+    while(i < n)
+    {
+        cout << a[i] << " ";
+        i++;
+    }
+    while(j < m)
+    {
+        cout << b[j] << " ";
+        j++;
+    }
+    cout << '\n';
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(6);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        rahul();
+    }
+    return 0;
+}
+
+
+
+
