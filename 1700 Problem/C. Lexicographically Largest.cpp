b7086bf15d4f90cfdad7ab44b0071d0fcd20ea83
Rahul Singh <85445665+rahulsingh522003@users.noreply.github.com>
2025-06-04 23:21:08 +0530

    Create C. Lexicographically Largest.cpp

---
+++ 1700 Problem/C. Lexicographically Largest.cpp
@@ -0,0 +1,92 @@
+// Problem Link - https://codeforces.com/problemset/problem/1930/C
+
+// Solution
+
+
+
+/* rsingh2003 */
+
+// Don't look at my code Bitch !!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// pbds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T>using oset = tree<T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>; // order_of_key  find_by_order
+
+#define int long long
+
+/*********************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*********************************************************************************************************/
+
+// I know it's hard, that's why I'm doing this. 
+
+void rahul()
+{
+    int n;
+    cin >> n;
+    vector<int>v(n+1);
+    for(int i = 1; i <= n; i++)
+        cin >> v[i];
+    vector<pair<int,int>>c;
+    for(int i = 1; i <= n; i++)
+        c.push_back({i+v[i],i});
+    sort(c.rbegin(), c.rend());
+    int val = 1e18;
+    for(int i = 0; i < n; i++)
+    {
+        debug(c[i].second)
+        if(val > c[i].first)
+        {
+            cout << c[i].first << " ";
+            val = c[i].first;
+        }
+        else
+        {
+            val--;
+            cout << val << " ";
+        }
+    }
+    cout << '\n';
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(6);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        rahul();
+    }
+    return 0;
+}
+
+
+
+
