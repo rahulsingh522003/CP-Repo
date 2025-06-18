cbe1c14fd92eead9011a715513189dd2e7fc0b58
Rahul Singh <85445665+rahulsingh522003@users.noreply.github.com>
2025-06-18 09:34:13 +0530

    Create E_Increasing Subsequences.cpp

---
+++ 1800/E_Increasing Subsequences.cpp
@@ -0,0 +1,97 @@
+//Problem Link - https://codeforces.com/problemset/problem/1922/E
+
+
+// solution
+
+
+/* rsingh2003 */
+
+// Don't look at my code Bitch !!!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// pbds
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class T>using oset = tree<T, null_type, less <T>, rb_tree_tag, tree_order_statistics_node_update>; // order_of_key  find_by_order
+
+#define int long long
+
+/*********************************************************************************************************/
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(int t) {cerr << t;}
+void _print(long double t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*********************************************************************************************************/
+
+// I know it's hard, that's why I'm doing this. 
+
+void answer(int x, int &mini, int &maxi, vector<int>&v)
+{
+    if(x == 0 || x == 1)
+        return;
+    if(x&1)
+    {
+        v.push_back(mini);
+        mini++;
+        answer(x-1,mini,maxi,v); 
+    }
+    else
+    {
+        v.push_back(maxi);
+        maxi--;
+        answer(x/2,mini,maxi,v);
+    }
+}
+
+void rahul()
+{
+    int x;
+    cin >> x;
+
+    vector<int>v;
+    int mini = -1e6;
+    int maxi = 1e6;    
+    answer(x,mini,maxi,v);
+    reverse(v.begin(),v.end());
+    cout <<v.size() << '\n';
+    for(auto &it : v)
+        cout << it << " ";
+    cout << '\n';
+}
+
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout << fixed << setprecision(6);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        rahul();
+    }
+    return 0;
+}
+
+
+
+
